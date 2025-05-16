f2cbbd653dc7da862895783691cc685d6c7a70ad
Yuuki <heinrichtester4@gmail.com>
2025-05-16 02:17:28 +0200

    seperated the image array into a different file

---
+++ data.js
@@ -0,0 +1,311 @@
+export const allImages = [
+  {
+    url: "https://pbs.twimg.com/media/Gq9KQHgWQAAlDQp?format=jpg&name=large",
+    tags: ["can you", "lock the fuck in", "angry", "pointing"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gq3iG4PWcAAI4He?format=jpg&name=large",
+    tags: [
+      "nice opinion",
+      "too bad",
+      "fucking stupid",
+      "sunglasses",
+      "joint",
+      "cap",
+    ],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gqjo6dCX0AEPIIE?format=png&name=900x900",
+    tags: ["pizza", "beer", "never kill yourself", "food"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp3-vyUWIAANkYX?format=jpg&name=large",
+    tags: ["smug", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp3-vzWWcAAQKsH?format=jpg&name=large",
+    tags: ["smug", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp3-vyQWoAAm1Gt?format=jpg&name=large",
+    tags: ["smug", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp3-vyIWcAAI1m7?format=jpg&name=large",
+    tags: ["smug", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp1MGy9XoAA-4mB?format=jpg&name=large",
+    tags: ["happy", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp1MGy9WQAAT3jE?format=jpg&name=large",
+    tags: ["happy", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp1LLbLXAAAZ7SC?format=jpg&name=large",
+    tags: ["happy", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp1KNt1XEAAwuMn?format=jpg&name=large",
+    tags: ["happy", "smile", "liyue"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp00lrzXAAAWZmO?format=jpg&name=large",
+    tags: ["middle finger", "flip off", "angry"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp0f4BoXkAAjA60?format=jpg&name=large",
+    tags: ["gun", "threatening", "denial", "aether"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gp0aCVYXgAAHSke?format=jpg&name=medium",
+    tags: ["dont call me pookie", "im a gangsta", "smug"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gpy7qg9WMAAZpsV?format=jpg&name=large",
+    tags: ["happy", "hand heart", "love", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gpv_b3lWIAA8_BC?format=jpg&name=large",
+    tags: ["smug", "sandwich", "food"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gpv5vSJXIAAjWNr?format=jpg&name=large",
+    tags: ["happy", "rice", "food", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpqWV7WWAAE7UWt?format=jpg&name=large",
+    tags: ["pokemon", "fennekin"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpqTgsFWkAA09nI?format=jpg&name=large",
+    tags: ["hug", "cactus", "friends", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpqD10aXYAAiFLt?format=jpg&name=large",
+    tags: ["happy", "cookie", "eating", "cute", "food"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gpp62FAWQAA0rhC?format=png&name=small",
+    tags: ["sad", "sob", "im too dumb"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GplUpXwWwAA9M9I?format=jpg&name=large",
+    tags: ["yes", "yes i did", "phineas and ferb", "happy", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GplNQK2X0AAuHut?format=jpg&name=large",
+    tags: ["i have a very particular set of skills", "phone", "calling"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GplDs2WWoAA5DlO?format=jpg&name=large",
+    tags: ["built different", "muscle", "buff"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GplBZZ8WUAAj1_L?format=jpg&name=large",
+    tags: ["thats the neat part", "you dont", "invincible", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gpk9rCBWgAAmfrn?format=jpg&name=large",
+    tags: ["okay get in", "coffin", "spongebob", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpjW5noWEAAG5D9?format=jpg&name=large",
+    tags: ["sad", "i no longer feel", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpjSicgWQAAX1hs?format=jpg&name=large",
+    tags: ["never cook again", "i kinda agree"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpUGKQPWQAAlRkW?format=jpg&name=large",
+    tags: ["thousand yard stare", "soldier", "war", "traumatized"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpKlgfkWkAAeEVD?format=jpg&name=medium",
+    tags: ["cashier", "cash or card", "bored"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GpGz5tUXgAAEKA7?format=jpg&name=large",
+    tags: ["art", "mona lisa"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GoyBDXyWcAAVWfr?format=jpg&name=large",
+    tags: ["gaming", "minecraft"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GoDQvqlXAAA57zD?format=jpg&name=large",
+    tags: ["bait", "mental retardation", "call it", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GneEJPPW0AADMEY?format=png&name=900x900",
+    tags: ["yanfei", "yantao", "body swap"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gndb_U1WsAAaKLc?format=jpg&name=large",
+    tags: ["stare", "cute", ":3", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnajowCWsAEXEvJ?format=jpg&name=small",
+    tags: ["laughing", "stare", "funny", "XD"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnaNXECXIAAEBA5?format=jpg&name=large",
+    tags: ["laptop", "stare", "sad", "angry"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnaBnVXWIAAOuo1?format=jpg&name=large",
+    tags: ["talking", "explain", "listen here"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnaKnKmWIAAaGu9?format=jpg&name=large",
+    tags: ["praying", "please", "smug", "smile"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnZ-RmpXcAA7Fk9?format=jpg&name=large",
+    tags: ["depressed", "sad", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnZ9IucWIAA9O1-?format=jpg&name=large",
+    tags: ["carrying box", "bashame", "shikanoko", "My Deer Friend Nokotan"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnQTMggXgAErejS?format=jpg&name=large",
+    tags: ["punch", "angry"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnP3Q5AXAAALC2m?format=png&name=360x360",
+    tags: ["retro game", "great but", "metal gear solid"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnPMw1IWUAAUGg2?format=png&name=900x900",
+    tags: ["trade offer", "primogems", "yahoo"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnLaScmXQAAyvIn?format=jpg&name=large",
+    tags: [
+      "yanfei",
+      "yantao",
+      "you look lonely",
+      "i can fix that",
+      "blade runner",
+      "yuri",
+    ],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnLNZLDWoAA21dt?format=jpg&name=large",
+    tags: ["sad", "crying", "rain"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnEJUSZW0AAQ7yO?format=jpg&name=large",
+    tags: ["sad", "prison", "jail", "arrest"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnLEYJfWwAAflzd?format=jpg&name=medium",
+    tags: ["games", "phone", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnF6m4DWYAAnFKY?format=jpg&name=large",
+    tags: [
+      "bright side",
+      "dark side",
+      "suicide",
+      "hanging yourself",
+      "sad",
+      "happy",
+    ],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnF1eGRW4AABncH?format=jpg&name=large",
+    tags: ["honkai star rail", "headpat"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnFXn_NXkAIISLI?format=jpg&name=medium",
+    tags: ["yanfei", "yantao", "she wants to order", "yuri"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnE0P7aXYAA9_d3?format=jpg&name=large",
+    tags: ["wait", "don't unfollow", "sad"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GnDOJ49WgAE_u_M?format=jpg&name=large",
+    tags: ["qiqi", "neuron activation", "monkey"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm6gT7aWoAAH9ZG?format=jpg&name=large",
+    tags: ["drinking", "shocked", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm6cFd6WgAAwjWT?format=jpg&name=large",
+    tags: ["wait a minute", "smug", "smile", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm6aKOoWoAAkuhA?format=jpg&name=large",
+    tags: ["you get what you fucking deserve", "joker"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm6U17XXQAA36na?format=jpg&name=large",
+    tags: ["segs", "sex", "bed", "pointing", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm5yUYIXgAAd0SL?format=jpg&name=large",
+    tags: ["pointing", "that's me", "Leonardo DiCaprio", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm18UeeWcAA2Av1?format=jpg&name=large",
+    tags: ["arlecchino", "hug", "friends", "happy"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm11qasW8AAsD2t?format=jpg&name=4096x4096",
+    tags: ["yay nay", "nah yeah", "drake", "compare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gm1L0LKXEAAXasX?format=png&name=900x900",
+    tags: ["pp", "please respond", "stare"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GmxcTT8bcAA3PWT?format=jpg&name=large",
+    tags: ["yanfei", "yantao", "kiss", "yuri"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GmxUD44boAAearo?format=jpg&name=large",
+    tags: ["qiqi", "rocket launcher", "murder", "team fortress 2", "soldier"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GmxK5nHXAAE_pOJ?format=jpg&name=large",
+    tags: ["wedding", "running", "aether", "escape", "disgust", "denial"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gmw-nNBWwAAzmJY?format=jpg&name=large",
+    tags: ["reading", "book", "confused", "programming"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gmw48gfWIAAB8MV?format=jpg&name=large",
+    tags: ["krusty krab", "burger", "spongebob", "food"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gmwv3XIbIAAmiSy?format=jpg&name=large",
+    tags: ["police", "tax evasion", "arrest"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gmwp4cvX0AA2XUD?format=jpg&name=large",
+    tags: ["sleepy", "tired", "defeat", "bed", "league of legends"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gmwd18fbsAA8od-?format=jpg&name=large",
+    tags: ["boxing", "knock out", "aether"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GmwX5DwXQAAATeI?format=jpg&name=large",
+    tags: ["kiss", "disgust", "aether", "no", "denial"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/GmmatHuasAAmWwx?format=jpg&name=large",
+    tags: ["hug", "furina", "happy"],
+  },
+  {
+    url: "https://pbs.twimg.com/media/Gmk_bI0WAAEa9xT?format=jpg&name=medium",
+    tags: ["reading", "bible", "book", "happy"],
+  },
+];
